04e9d8cce4a78d9317cb8091eda0410375e05165
vanesaandni <vanesakuncoro@gmail.com>
2024-11-06 20:13:06 +0700

    update

--- lib/src/data/movie.dart
+++ lib/src/data/movie.dart
@@ -2,14 +2,14 @@ import 'package:paml_flutter_responsi/src/models/movie.dart';
 
 final List<Movie> movieData = [
   Movie(
-    imageUrl: "https://www.themoviedb.org/t/p/original/xYmoFzsH1R74msULgSFWH2foMtu.jpg",
+    imageUrl: "https://example.com/movie1.jpg",
     title: "Ancika",
     description: "Film ini bergenre romantis. Berceritakan mengenai kisah cinta baru yang dimulai setelah putus dengan mantannya.",
     duration: 143,
     date: DateTime.now().subtract(const Duration(days: 360)),
   ),
   Movie(
-    imageUrl: "https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/chameleon/title-movie/238840_SILENT%20HILL_1400x2100_Eng.jpg?itok=cV5B_OHO",
+    imageUrl: "https://example.com/movie2.jpg",
     title: "Silent Hill",
     description: "Film ini bergenre horror. Bercerita tentang kota yang terbakar karena anak yang dipercaya penyihir.",
     duration: 85,

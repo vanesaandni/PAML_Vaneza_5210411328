461fe165eb601ae1fa178362e27090f8b4c11187
vanesaandni <vanesakuncoro@gmail.com>
2024-11-06 19:01:39 +0700

    update

--- lib/src/data/music.dart
+++ lib/src/data/music.dart
@@ -3,27 +3,27 @@ import 'package:paml_flutter_responsi/src/models/music.dart';
 final List<Music> musicData = [
   Music(
     imageUrl: "https://cdns.klimg.com/resized/670x/g/8/_/8_fakta_tersembunyi_tentang_eren_yeager_yang_jarang_diketahui_penggemar_ungkap_sisi_lain_sang_protagonis/p/eren_yaeger-20240830-006-non_fotografer_kly.jpg",
-    artist: "Artist 1",
-    song: "Song Title 1",
+    artist: "The 1975",
+    song: "About You",
   ),
   Music(
     imageUrl: "https://example.com/song2.jpg",
-    artist: "Artist 2",
-    song: "Song Title 2",
+    artist: "The 1975",
+    song: "Robbers",
   ),
   Music(
     imageUrl: "https://example.com/song3.jpg",
-    artist: "Artist 3",
-    song: "Song Title 3",
+    artist: "Pamungkas",
+    song: "I Love About You",
   ),
   Music(
     imageUrl: "https://example.com/song3.jpg",
-    artist: "Artist 3",
-    song: "Song Title 3",
+    artist: "Pamungkas",
+    song: "Putus",
   ),
   Music(
     imageUrl: "https://example.com/song3.jpg",
-    artist: "Artist 3",
-    song: "Song Title 3",
+    artist: "The Maroon 5",
+    song: "Sunday Morning",
   ),
 ];
